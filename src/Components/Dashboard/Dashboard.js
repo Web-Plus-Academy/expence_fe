b129136a70ec0419f0077f73fbfb8f754c27356b
saranthecoder <115061220+saranthecoder@users.noreply.github.com>
2025-03-27 10:35:05 +0530

    unused

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 import { useGlobalContext } from '../../context/globalContext';
 import History from '../../History/History';
 import { InnerLayout } from '../../styles/Layouts';
-import { dollar } from '../../utils/Icons';
 import Chart from '../Chart/Chart';
 
 function Dashboard() {
